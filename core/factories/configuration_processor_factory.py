4c65e7ae9d072476d6ea91bcd6ea467356f6012b
Maksim, Eremenko <erjomenko@gmail.com>
2025-01-15 11:56:11 -0500

    add ConfigurationFileProcessor1D

--- core/factories/configuration_processor_factory.py
+++ core/factories/configuration_processor_factory.py
@@ -10,6 +10,7 @@ Created on Tue Oct 29 14:50:11 2024
 from interfaces.base_interfaces import IConfigurationProcessorFactory
 from processors.rmc6f_processor import RMC6fProcessor
 from processors.configuration2d_file_processor import ConfigurationFileProcessor2D
+from processors.configuration1d_file_processor import ConfigurationFileProcessor1D
 
 from processors.rmc6f_average_structure_calculator import RMC6fAverageStructureCalculator
 from processors.rmc6f_average_structure_reader import RMC6fAverageStructureReader
@@ -38,11 +39,16 @@ class Processor2DFactory(IConfigurationProcessorFactory):
     def create_processor(self, file_path: str, processor_type: str = 'read', average_file_path: str = None):
         return ConfigurationFileProcessor2D(file_path)
 
+class Processor1DFactory(IConfigurationProcessorFactory):
+    def create_processor(self, file_path: str, processor_type: str = 'read', average_file_path: str = None):
+        return ConfigurationFileProcessor1D(file_path)
+
 class ConfigurationProcessorFactoryProvider:
     _factories = {
         'rmc6f': RMC6fProcessorFactory(),
         'hdf5': HDF5ProcessorFactory(),
         'f2d': Processor2DFactory(),
+        'f1d': Processor1DFactory(),
         # Add more factories for other configuration file types as needed
     }
 
